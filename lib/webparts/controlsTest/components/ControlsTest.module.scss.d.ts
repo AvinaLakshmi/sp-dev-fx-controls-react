3562dbedb3355ad67b0f592c63d435c497fefe32
unknown <luckyavinajul03@yahoo.co.in>
2020-09-09 15:13:40 +0000

    updated

---
+++ lib/webparts/controlsTest/components/ControlsTest.module.scss.d.ts
@@ -0,0 +1,13 @@
+declare const styles: {
+    controlsTest: string;
+    container: string;
+    row: string;
+    listItem: string;
+    button: string;
+    label: string;
+    siteBreadcrumb: string;
+    carouselContent: string;
+    carouselButtonsContainer: string;
+    carouselImageContent: string;
+};
+export default styles;
---
+++ lib/webparts/controlsTest/components/IControlsTestProps.d.ts
@@ -0,0 +1,31 @@
+/// <reference types="react" />
+import { WebPartContext } from '@microsoft/sp-webpart-base';
+import { ImageSize } from '../../../FileTypeIcon';
+import { DisplayMode } from '@microsoft/sp-core-library';
+import { IProgressAction } from '../../../Progress';
+import { IFilePickerResult } from '../../../FilePicker';
+export interface IControlsTestProps {
+    context: WebPartContext;
+    description: string;
+    title: string;
+    displayMode: DisplayMode;
+    updateProperty: (value: string) => void;
+    totalPages?: number;
+}
+export interface IControlsTestState {
+    imgSize: ImageSize;
+    items: any[];
+    initialValues: any[];
+    iFrameDialogOpened?: boolean;
+    iFramePanelOpened?: boolean;
+    authorEmails: string[];
+    selectedList: string;
+    progressActions: IProgressAction[];
+    currentProgressActionIndex?: number;
+    dateTimeValue: Date;
+    richTextValue: string;
+    currentCarouselElement: JSX.Element;
+    canMovePrev: boolean;
+    canMoveNext: boolean;
+    filePickerResult?: IFilePickerResult;
+}
